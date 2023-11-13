6f90186719ec6962da8baac43f9a117b9417f507
balaka177 <122111835+balaka177@users.noreply.github.com>
2023-11-13 18:58:29 +0530

    Add files via upload
    Its time to make money

---
+++ controller/premium.js
@@ -0,0 +1,71 @@
+const Razorpay = require('razorpay');
+const orders=require('../model/order');
+const jwt=require('jsonwebtoken');
+const Order = require('../model/order');
+
+
+exports.purchage_premium=async (req,res,next)=>{
+try{    
+        var rzp=new Razorpay({
+            key_id:process.env.RAZORPAY_KEY_ID,
+            key_secret:process.env.RAZORPAY_KEY_SECRET,
+        })
+        const amount=2500;
+        rzp.orders.create({amount,currency:"INR"},(err,order)=>{
+            if(err){
+                throw new Error(JSON.stringify(err));
+            }
+            //console.log(order.id)
+            req.user.createOrder({orderid:order.id,status:'PENDING'}).then(()=>{
+                return res.status(201).json({order,key_id:rzp.key_id});
+            }).catch(err =>{
+                throw new Error(err);
+            })
+        })
+    }catch(err){
+        console.log(err)
+    }
+}
+
+exports.update_purchase=async(req,res,next)=>{
+    try{
+        
+        const {payment_id,order_id}=req.body;
+        console.log('payment id:',payment_id)
+        if(payment_id===null){
+            Order.findOne({where:{ orderid:order_id}}).then(order =>{
+                order.update({paymentid:payment_id,status:'FAILED'}).then(() =>{
+                    req.user.update({isPrimeUser:false}).then(()=>{
+                        return res.status(202).json({success: false,message:'Transaction Failed'});
+                    }).catch((err)=>{
+                        throw new Error(err)
+                    })
+                }).catch((err)=>{
+                    throw new Error(err)
+                })
+            }).catch((err)=>{
+                throw new Error(err)
+            })
+        
+        }
+        else{
+        Order.findOne({where:{orderid:order_id}}).then(order =>{
+            order.update({paymentid:payment_id,status:'SUCCESSFUL'}).then(() =>{
+                req.user.update({isPrimeUser:true}).then(()=>{
+                    return res.status(202).json({success: true,message:'Transaction Successful'});
+                }).catch((err)=>{
+                    throw new Error(err)
+                })
+            }).catch((err)=>{
+                throw new Error(err)
+            })
+        }).catch((err)=>{
+            throw new Error(err)
+        })
+    }
+    }catch(err){
+        console.log(err)
+    }
+
+
+}
